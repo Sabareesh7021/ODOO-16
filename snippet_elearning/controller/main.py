8a41769afdaeae30b144ce4fd6df996d65d9b2ea
Sabareesh7021 <118870534+Sabareesh7021@users.noreply.github.com>
2023-01-09 12:26:41 +0530

    Add files via upload

--- snippet_elearning/controller/main.py
+++ snippet_elearning/controller/main.py
@@ -5,5 +5,6 @@ from odoo.http import request
 class Sales(http.Controller):
     @http.route(['/elearning_snippet'], type="json", auth="public")
     def elearning_snippet(self):
-        elearning = request.env['slide.channel'].sudo().search_read([('website_published', '=', True)], ['name', 'image_1920', 'id', 'description'], order='create_date desc')
+        elearning = request.env['slide.channel'].sudo().search_read([('website_published', '=', True)], ['name', 'image_1920', 'id', 'description'], order='create_date desc'
+                                                                    )
         return elearning
